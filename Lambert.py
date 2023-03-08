d26d82b06f3c0611bee21c5aad1a8a385d6f6d59
Jacob Johnson <jacobkj314@gmail.com>
2023-03-08 09:51:49 -0700

    completed initial implementation and added examples

--- Lambert.py
+++ Lambert.py
@@ -40,6 +40,8 @@ class Set:
 		return self._set.issubset(other._set)
 	def issuperset(self, other):
 		return self._set.issuperset(other._set)
+	def union(self, other):
+		return Set(*(self._set.union(other._set)))
 
 	def add(self, e):
 		self._set.add(e)
@@ -75,14 +77,14 @@ def dictUnion(a, b):
 	return ans
 
 def f(w, k=K): #get attested factors
-	return Set(*(w[i:i+j] for i in range(len(w) - k + 1) for j in range(k+1)))
+	return Set(*(w[i:i+j] for j in range(k+1+1) for i in range(len(w) - j + 1))) #first range is 0 to k+1 inclusive, second is from the start of the word to the end
 def x(w, k=K): #get attested augmented subsequences, encoded as a dict from tuples to Sets of Sets
 	ansSet = set()
-	for j in range(1,k):#iterate across factor lengths
+	for j in range(1,k+1):#iterate across factor lengths - 1 to k inclusive
 		for xi in list(combinations(range(len(w)), j)):#look at each subsequence of indices
 			qi = tuple(map(lambda x : w[x], xi))#look at the symbols at those indices
 			ii = Set(*(map(lambda x : w[x], [i for i in range(xi[0],xi[-1]) if i not in xi])))#look at the symbols at the intervening indices
-			if len(set(qi).intersection(set(ii))) == 0:#if there is no overlap, it is a valid subsequence
+			if len(set(qi).intersection(set(ii))) == 0:#if there is no overlap in terms of selected characters, it is a valid subsequence
 				ansSet.add((qi, ii))
 	ansDict = {() : Set(Set())}
 	for q, i in ansSet:
@@ -107,27 +109,56 @@ def r(asubs): #restrict augmented subsequences
 				new[q].add(j)
 	return new
 
-
+def grammar(k=K):
+	return (Set(), dict(), k)
+def learn_step(g, w):
+	gl, gs, k = g
+	return (gl.union(f(w, k=k)), r(dictUnion(gs, x(w, k=k))), k)
 def learn(samples, k=K):
-	gl = Set()
-	gs = dict()
-
+	g = grammar(k)
 	for w in samples:
-		gl.update(f(w, k=k))
-		gs = r(dictUnion(gs, x(w, k=k)))
-	return (gl, gs)
-
+		g = learn_step(g, w)
+	return g
 def scan(g, w):
-	gl, gs = g
-	qi = r(dictUnion(gs, x(w)))
-	return f(w).issubset(gl) and all(qi[j].issubset(gs[j]) for j in qi)
-
-
+	gl, gs, k = g
+	qi = r(dictUnion(gs, x(w, k=k)))
+	return f(w, k=k).issubset(gl) and all(((j in gs) and (qi[j].issubset(gs[j]))) for j in qi)
 
 
 #This is a dummy TSL-2 grammar for testing
-from random import getrandbits, randint
+#the tier consists of odd digits, which cannot "decrease" through the word. So 11111, 33333, 99999, are all ok, 13339 is ok, but 75 is NOT
+from random import randint
 def generate():
+	w = ''
+	odd = 0
+	for _ in range(randint(5, 12)):
+		incr = randint(0,1)
+		if randint(0,1):#even
+			w += str(randint(0,4)*2)
+		else:#odd
+			if incr and odd < 4:
+				odd += 1
+			w += str(odd*2 + 1)
+	return w
+def accept(w):
+	odd = 1; even = 0
+	for s in w:
+		s = int(s)
+
+		if s % 2 == 1:
+			if s < odd:
+				return False
+			odd = s
+		else:
+			continue
+	return True
+
+
+
+#This is a dummy MTSL-2 grammar for testing
+#It has a second tier consisting of even digits with the same property as above, so 00000, 44444, 88888 are ok, as is 02468, but 20 is NOT ok.
+from random import randint
+def generate_mtsl():
 	w = ''
 	odd = 0; even = 0
 	for _ in range(randint(5, 12)):
@@ -141,4 +172,73 @@ def generate():
 				odd += 1
 			w += str(odd*2 + 1)
 	return w
+def accept_mtsl(w):
+	odd = 1; even = 0
+	for s in w:
+		s = int(s)
 
+		if s % 2 == 1:
+			if s < odd:
+				return False
+			odd = s
+		else:
+			if s < even:
+				return False
+			even = s
+	return True
+
+#This is another, more complicated dummy MTSL-2 grammar for testing
+#This grammar adds a 3rd tier, consisting of 0369, which also must occur in "increasing" order 
+from random import randint
+def generate_mtsl_b():
+	w = ''
+	odd = 0; even = 0; three = 0
+	for _ in range(randint(5, 12)):
+		incr = randint(0,1)
+		if randint(0,1):#even
+			if incr and even < 4:
+				even += 1
+			next = even*2
+		else:#odd
+			if incr and odd < 4:
+				odd += 1
+			next = odd*2 + 1
+		if next%3 == 0:
+			if next < three:
+				if next%2 == 0:
+					if even < 4:
+						even += 1
+				else:
+					if odd < 4:
+						odd += 1
+				continue
+			three = next
+		w += str(next)
+	return w
+def accept_mtsl_b(w):
+	odd = 1; even = 0; three = 0
+	for s in w: #check even and odd requirements
+		s = int(s)
+
+		if s % 2 == 1:
+			if s < odd:
+				return False
+			odd = s
+		else:
+			if s < even:
+				return False
+			even = s
+	for s in w: #check three requirements
+		s = int(s)
+		if s % 3 == 0:
+			if s < three:
+				return False
+			three = s
+	return True
+
+#This is the example from the original paper!
+sample = ['akkalkak','klark','kralk','karlakalra','akrala','aklara','rakklarkka','arkralkla','laarlraalr','kaaakkrka','klkkklrk','krlkrkl','alrla']
+
+#this is a mtsl example adapted from the original paper's example
+import re
+sample2 = sample + [re.sub('a', 'e', w) for w in sample] #replace all as with es and also add them to the sample, so now there is a second vowel harmony tier
