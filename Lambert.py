d3bc5bf25650dcfc6cbbdea8e605ecb878681126
Jacob Johnson <jacobkj314@gmail.com>
2023-03-20 20:04:37 -0600

    added experimental patterns

--- Lambert.py
+++ Lambert.py
@@ -3,6 +3,14 @@ This is just my scratch paper for testing out ideas from Lambert, D. (2021, Augu
 Retrieved from https://proceedings.mlr.press/v153/lambert21a/lambert21a.pdf
 '''
 
+def nsorted(collection): #implement numerical sorting, rather than default lexicographical sorting
+	return sorted(collection, key = lambda element : (len(element), element))
+def star(sigma, maxLen):
+	result = []
+	for count in range(maxLen+1):
+		result += list(filter(lambda *x:True, [''.join(w) for w in product(*[sigma for _ in range(count)])]))
+	return result
+
 class Set:
 	'''
 	This is just a wrapper class around python's set class, which allows Sets to be placed in other Sets
@@ -10,22 +18,29 @@ class Set:
 	def __init__(self, *_set):
 		self._set = set(_set)
 		self._rehash()
+		self._ordered = False; self._order()
 
 	def _rehash(self):
-		self._hash = tuple(sorted(self._set)).__hash__()
+		self._hash = tuple(nsorted(self._set)).__hash__()
 	def __hash__(self):
 		if self._hash is None:
 			self._rehash()
 		return self._hash
+	
+	def _order(self):
+		if not self._ordered:
+			self._set = {e for e in nsorted(self._set)}
+			self._ordered = True
 
 	def __eq__(self, other):
 		return self.__class__ == other.__class__ and self._set == other._set
 	def __len__(self):
 		return len(self._set)
-	def __str__(self):
-		return r'{}' if len(self) == 0 else self._set.__str__()
 	def __repr__(self):
-		return r'{}' if len(self) == 0 else self._set.__repr__()
+		self._order()
+		return '{{{0}}}'.format(', '.join(map(repr, nsorted(self._set))))
+	def __str__(self):
+		return self.__repr__()
 	def __lt__(self, other):
 		if other.__class__ == self.__class__:
 			return self._set.__lt__(other._set)
@@ -53,14 +68,16 @@ class Set:
 
 	def add(self, e):
 		self._set.add(e)
-		self._hash = None
+		self._hash = None; self._ordered = False
 	def update(self, es):
 		self._set.update(es)
-		self._hash = None
+		self._hash = None; self._ordered = False
 	# I don't need methods to remove elements
 
+def ordered(d):
+	return {k:d[k] for k in nsorted(d)}
 
-K = 2
+K = 2; M = 1
 
 from itertools import combinations
 
@@ -80,25 +97,34 @@ def dictUnion(a, b):
 		ans[e].update(b[e])
 	return ans
 
-def f(w, k=K): #get attested factors
+def f(w, k=K, m=M): #get attested factors
+	w = itsl(w, m=m)
 	return Set(*(w[i:i+j] for j in range(k+1+1) for i in range(len(w) - j + 1))) #first range is 0 to k+1 inclusive, second is from the start of the word to the end
-def x(w, k=K): #get attested augmented subsequences, encoded as a dict from tuples to Sets of Sets
+def f(w, k=K, m=M): #get attested factors
+	w = itsl(w, m=m)# # # # # Added new *m:m to skip ahead, ignoring overlaps
+	# # #return Set(*(w[i:i+j*m:m] for j in range(k+1+1) for i in range(len(w) - j + 1))) #first range is 0 to k+1 inclusive, second is from the start of the word to the end
+	return Set(*(w[i:i+j] for j in range(k+1+1) for i in range(len(w) - j + 1))) #first range is 0 to k+1 inclusive, second is from the start of the word to the end
+def x(w, k=K, m=M): #get attested augmented subsequences, encoded as a dict from tuples to Sets of Sets
 	"""
 	This function retrieves the set of augmented subsequences and encodes them as a dict from tuples of symbols (subsequeces) to Sets of Sets (the inner sets are the intervener sets, while the outer sets are the sets of intervener sets)
 	"""
+
+	w=itsl(w, m=m) #first break the string into m-width symbols
+	m=1 # # # # # temporarily undoing changes
 	ansSet = set()
-	for j in range(1,k+1):#iterate across factor lengths - 1 to k inclusive
+	for j in range(1,k+1):#iterate across factor lengths, 1 to k inclusive
 		for xi in list(combinations(range(len(w)), j)):#look at each subsequence of indices
-			qi = tuple(map(lambda x : w[x], xi))#look at the symbols at those indices
-			ii = Set(*(map(lambda x : w[x], [i for i in range(xi[0],xi[-1]) if i not in xi])))#look at the symbols at the intervening indices
-			if len(set(qi).intersection(set(ii))) == 0:#if there is no overlap in terms of selected characters, it is a valid subsequence
-				ansSet.add((qi, ii))
+			if True:# # # # #all(map(lambda pair: pair[1]-pair[0] >= m, combinations(xi,2))):#only proceed if no pair of selected m-width symbols overlap
+				qi = tuple(map(lambda x : w[x], xi))#look at the symbols at those indices
+				ii = Set(*(map(lambda x : w[x], [i for i in range(xi[0],xi[-1]) if all(abs(y-i) >=m for y in xi)])))#look at the symbols at the intervening and non-overlapping indices
+				if len(set(qi).intersection(set(ii))) == 0:#if there is no overlap in terms of selected characters, it is a valid subsequence
+					ansSet.add((qi, ii))
 	ansDict = {() : Set(Set())}
 	for q, i in ansSet:
 		if ansDict.get(q) is None:
 			ansDict[q] = Set()
 		ansDict[q].add(i)
-	return ansDict
+	return ordered(ansDict)
 def r(asubs): 
 	"""
 	this function restricts augmented subsequences, keeping only the smallest ones needed
@@ -119,25 +145,38 @@ def r(asubs):
 				new[q].add(j)
 	return new
 
-def grammar(k=K):
+
+
+
+
+
+
+
+
+
+def itsl(w, m=M):
+	return tuple([w[i:i+m] for i in range(len(w)-m+1)])
+
+def grammar(k=K,m=M):
 	"""
 	Returns a blank grammar (rejecting every string) that can be used as a starting point for building grammars from input
 	The grammar consists of a Set() (of attested factors) a dict() (representing a set of augmented subsequences) and an int (the tier window width)
 	"""
-	return (Set(), dict(), k)
+	return (Set(), dict(), (k,m))
 def learn_step(g, w):
 	'''
 	Given an input grammar g and a string w, performs one online learning step
 	'''
-	gl, gs, k = g
-	return (gl.union(f(w, k=k)), r(dictUnion(gs, x(w, k=k))), k)
-def learn(samples, k=None, g=None):
+	gl, gs, (k,m) = g
+	return (gl.union(f(w, k=k,m=m)), ordered(r(dictUnion(gs, x(w, k=k,m=m)))), (k,m))
+def learn(samples, k=None,m=None, g=None):
 	'''
 	creates a new grammar or takes an existing grammar
 	applies learn_step to all strings in samples 
 	'''
-	k = k if k else (g[2] if g else K)
-	g = g if g else grammar(k)
+	k = k if k else (g[2][0] if g else K)
+	m = m if m else (g[2][1] if g else M)
+	g = g if g else grammar(k,m)
 	for w in samples:
 		g = learn_step(g, w)
 	return g
@@ -145,9 +184,9 @@ def scan(g, w):
 	'''
 	given a grammar g and a string w, returns whether the given grammar accepts w
 	'''
-	gl, gs, k = g
-	qi = r(dictUnion(gs, x(w, k=k)))
-	return f(w, k=k).issubset(gl) and all(((j in gs) and (qi[j].issubset(gs[j]))) for j in qi)
+	gl, gs, (k,m) = g
+	qi = r(dictUnion(gs, x(w, k=k,m=m)))
+	return f(w, k=k,m=m).issubset(gl) and all(((j in gs) and (qi[j].issubset(gs[j]))) for j in qi)
 
 
 #This is a dummy TSL-2 grammar for testing
@@ -295,6 +334,86 @@ def accept_mtsl_blockers(w):
 	return True
 
 
+def generate_itsl():
+	c = 'd' if randint(0,1) else 'n'
+	w = ''
+	for _ in range(randint(5,12)):
+		if randint(1,2) == 1:
+			if randint(1,2) == 1:
+				w += 'nd'
+			else:
+				w += c
+		else:
+			w += 'x'
+	return w
+def accept_itsl(w):
+	w = re.sub('nd','',w)
+	w = re.sub('x','',w)
+	return w in [x * len(w) for x in ('n','d')]
 
-def failures(grammar, acceptor, sample):
-	return [x for x in sample if not (lambda w : acceptor(w) == scan(grammar,w))(x)]
+#This is supposed to help automate testing
+pointers = []
+from itertools import product
+def failures(accept, sigma, count=None, k=K, m=M):
+	count = count if count else (k+1)*(m+1)
+	sigmastar = star(sigma, (k+1)*(m+1))
+	sample = list(filter(lambda *x: accept(*x), sigmastar))
+	g = learn(sample, k=k,m=m)
+	pointers.clear()
+	pointers.extend([sigmastar, sample, g])
+	return [(w, a, t) for w in star(sigma, count) if ((a := accept(w)) != (t:= scan(g,w)))]
+
+#it is still failing on itsl, I haven't figured out how to ignore overlapping m-width symbols yet...
+rejects = failures(accept_itsl, 'ndx')
+
+
+
+sh = ['SaSekuS', 'sasokos', 'SakuSuS', 'sakesas',
+'SeSukuS', 'sesukos', 'SekeSuS', 'sekoses',
+'SiSokeS', 'sisekos', 'SikiSoS', 'sikisis',
+'SoSokeS', 'sosakos', 'SokaSeS', 'sokusas',
+'SuSakiS', 'susukos', 'SukoSoS', 'sukasus',
+'SaSokuS', 'sasakus', 'SakuSiS', 'sakisos',
+'SeSokiS', 'sesukis', 'SekeSoS', 'sekeses',
+'SiSukoS', 'sisokos', 'SikeSaS', 'sikasos',
+'SoSokuS', 'sosikas', 'SokeSiS', 'sokosis',
+'SuSakeS', 'susokis', 'SukoSeS', 'sukesas']
+
+fl = ['SasokuS', 'saSakus', 'SaSekuS', 'sasokos',
+'SesokiS', 'seSukis', 'SeSukuS', 'sesukos',
+'SisukoS', 'siSokos', 'SiSokeS', 'sisekos',
+'SosokuS', 'soSikas', 'SoSokeS', 'sosakos',
+'SusakeS', 'suSokis', 'SuSakiS', 'susukos',
+'SakusiS', 'sakiSos', 'SakuSuS', 'sakesas',
+'SekesoS', 'sekeSes', 'SekeSuS', 'sekoses',
+'SikesaS', 'sikaSos', 'SikiSoS', 'sikisis',
+'SokesiS', 'sokoSis', 'SokaSeS', 'sokusas',
+'SukoseS', 'sukeSas', 'SukoSoS', 'sukasus']
+
+ifl = ['SasokuS', 'saSakus', 'SakusiS', 'sakiSos',
+'SesokiS', 'seSukis', 'SekesoS', 'sekeSes',
+'SisukoS', 'siSokos', 'SikesaS', 'sikaSos',
+'SosokuS', 'soSikas', 'SokesiS', 'sokoSis',
+'SusakeS', 'suSokis', 'SukoseS', 'sukeSas',
+'SasekuS', 'saSokos', 'SakusuS', 'sakeSas',
+'SesukuS', 'seSukos', 'SekesuS', 'sekoSes',
+'SisokeS', 'siSekos', 'SikisoS', 'sikiSis',
+'SosokeS', 'soSakos', 'SokaseS', 'sokuSas',
+'SusakiS', 'suSukos', 'SukosoS', 'sukaSus']
+
+test = ['sekoSos', 'SekoSos', 'sukisas', 'sukisaS', 'SoSukoS', 'SosukoS',
+'SasokaS', 'Sasokas', 'SeSekaS', 'seSekaS', 'sukesus', 'sukeSus',
+'suSekos', 'suSekoS', 'SokuSiS', 'SokuSis', 'sisakus', 'siSakus',
+'SikisaS', 'sikisaS', 'sisokus', 'Sisokus', 'SakaSoS', 'SakasoS',
+'seSokos', 'seSokoS', 'SekeSaS', 'SekeSas', 'SokuSoS', 'SokusoS',
+'SakosaS', 'sakosaS', 'SoSukiS', 'soSukiS', 'susekus', 'suSekus',
+'sukeSos', 'SukeSos', 'sikosus', 'sikosuS', 'sikasus', 'sikaSus',
+'SisikaS', 'Sisikas', 'susikas', 'Susikas', 'SaSakoS', 'SasakoS',
+'SikuSis', 'sikuSis', 'sokasiS', 'sokasis', 'soSakas', 'sosakas',
+'Sesakis', 'SesakiS', 'SikoSis', 'SikoSiS', 'SusekiS', 'SuSekiS',
+'sokisoS', 'SokisoS', 'Sesikos', 'sesikos', 'sakuSes', 'sakuses',
+'saSekeS', 'saSekes', 'saSikuS', 'SaSikuS', 'SekosaS', 'SekoSaS',
+'Sosikos', 'SosikoS', 'Sosakis', 'sosakis', 'saSukes', 'sasukes',
+'siSukiS', 'siSukis', 'siSokiS', 'SiSokiS', 'SesokaS', 'SeSokaS',
+'sekasiS', 'SekasiS', 'sekisoS', 'sekisos', 'sokaSas', 'sokasas',
+'SakeSes', 'sakeSes', 'SakiSus', 'SakiSuS', 'SukesiS', 'SukeSiS']
