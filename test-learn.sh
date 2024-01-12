cc7de568eec5625b07c00ae93a0ab99befca8441
Jacob Johnson <jacobkj314@gmail.com>
2024-01-12 14:17:10 -0700

    ready for big experiments

--- test-learn.sh
+++ test-learn.sh
@@ -1,12 +1,12 @@
 for learner in 0 1
 do
-    for experiment in 0 2 4 5 6 8 9 10
-    do
-        for trial in 1 2 3 4 #need to extend to 10 total
-        do
-            python test-learn.py $learner $experiment $trial > big/transcripts/$learner-$experiment-$trial.txt &
-        done
-    done
+    #for experiment in 0 2 4 5 6 8 9 10
+    #do
+    #    for trial in 1 2 3 4 #need to extend to 10 total
+    #    do
+    #        python test-learn.py $learner $experiment $trial > big/transcripts/$learner-$experiment-$trial.txt &
+    #    done
+    #done
 
     for experiment in 1 3 7 
     do
