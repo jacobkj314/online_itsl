e18cce9df9a72048dc933432865943b57f15995b
Jacob Johnson <jacobkj314@gmail.com>
2024-01-13 07:51:28 -0700

    started artificial experiments

--- test-learn.sh
+++ test-learn.sh
@@ -2,7 +2,7 @@ for learner in 0 1
 do
     for experiment in 0 2 4 5 6 8 9 10
     do
-        for trial in 1 2 #3 4 5 6 7 8 9 10
+        for trial in 1 2 3 4 #5 6 7 8 9 10
         do
             python test-learn.py $learner $experiment $trial > big/transcripts/$learner-$experiment-$trial.txt &
         done
