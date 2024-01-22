2c410469ae1bbb0428b76d8ca5fbe4e86b9c9e66
Jacob Johnson <jacobkj314@gmail.com>
2024-01-22 10:56:42 -0700

    progress

--- final-clean.sh
+++ final-clean.sh
@@ -2,4 +2,4 @@ bash clean-big.sh
 bash test-eval.sh
 mv big experiments
 
-rm -rf particular* *random* *big* old* __pycache__ test* exceptions final-clean.sh
+rm -rf particular* *random* *big* old* __pycache__ exceptions final-clean.sh
