51ca9e89686ee61459f695699fed0c5e29ef1da9
Jacob Johnson <jacob.k.johnson@utah.edu>
2024-04-17 19:32:08 -0600

    rearranged experiment order for test-iverse-eval.sh

--- test-inverse-eval.sh
+++ test-inverse-eval.sh
@@ -1,6 +1,6 @@
 for learner in 0 1
 do
-    for experiment in 0 2 4 5 6 8 9 10  1 3 7
+    for experiment in 0 1 2 3 4 5 6 7 8 9 10
     do
         python test-inverse-eval.py $learner $experiment
     done
